8dde690e73d738dc7b6fabeff4f61232bb27d221
Zakaria <zknabdi@gmail.com>
2019-04-20 19:55:05 -0500

    clearing todo

--- app/src/main/java/com/persona/favoritetoys/MainActivity.java
+++ app/src/main/java/com/persona/favoritetoys/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Declare a TextView variable called mToysListTextView
+    // Completed (1) Declare a TextView variable called mToysListTextView
     private TextView mToysListTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO (3) Use findViewById to get a reference to the TextView from the layout
+        // Completed (3) Use findViewById to get a reference to the TextView from the layout
         mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
-        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+        // Completed (4) Use the static ToyBox.getToyNames method and store the names in a String array
         String toyNames[] = ToyBox.getToyNames();
-        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        // Completed (5) Loop through each toy and append the name to the TextView (add \n for spacing)
         for(String myToys: toyNames){
             mToysListTextView.append(myToys+"\n\n\n");
         }
